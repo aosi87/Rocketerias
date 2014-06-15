5a36bb9f9ac1eda460a33b6fe3aca656732520b8
Isaac Alcocer <aosi87@gmail.com>
2014-06-15 13:49:19 -0500

    Se generaron las bases del sistema, coreUtils y manejo de datos.

--- src/controller/FileUtilities.java
+++ src/controller/FileUtilities.java
@@ -62,99 +62,6 @@ public class FileUtilities {
         this.path = path;
     }
     
-    /**
-     * Con este metodo leeremos los archivos en modo de compatibilidad, es decir los archivos
-     * con extencion .xls o creados con Microsoft Office 2007 o anteriores.
-     * 
-     * @param Excel2003FileToRead ruta del archivo con terminacion xls, si null es recivido una excepcion es arrojada.
-     * @throws java.io.IOException 
-     */
-    public static void readXLSFile(FileInputStream Excel2003FileToRead) throws IOException {
-        HSSFWorkbook wb = new HSSFWorkbook(Excel2003FileToRead);
-        HSSFSheet sheet = wb.getSheetAt(0);
-        HSSFRow row;
-        HSSFCell cell;
-        Iterator rows = sheet.rowIterator();
-        while (rows.hasNext()) {
-            row = (HSSFRow) rows.next();
-            Iterator cells = row.cellIterator();
-            while (cells.hasNext()) {
-                cell = (HSSFCell) cells.next();
-                if (cell.getCellType() == HSSFCell.CELL_TYPE_STRING) {
-                    System.out.print(cell.getStringCellValue() + " ");
-                } else if (cell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC) {
-                    System.out.print(cell.getNumericCellValue() + " ");
-                } else {
-                    //U Can Handel Boolean, Formula, Errors
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    /**
-     * Con este metodo leeremos los archivos en modo de compatibilidad, es decir los archivos
-     * con extencion .xlsx o creados con Microsoft Office 2010 o posteriores.
-     * Algoritmo y funcionamiento de la libreria POI-3.10 para lectura de Excel:
-     *      Al generar un libro, le entregamos la ruta de el archivo con formato xlsx
-     *      una vez teniendo la instancia de el libro seleccionamos la hoja con la cual trabajar.
-     *      es decir (XSSSheet = 0 es la primera hoja), una vez seleccionada la hoja usamos las
-     *      estructuras de la libreria para obtener las filas, o celdas individualmente.
-     * 
-     * @param Excel2007FileToRead
-     * @throws IOException 
-     */
-    public static void readXLSXFile(FileInputStream Excel2007FileToRead) throws IOException{
-        XSSFWorkbook xwb = new XSSFWorkbook(Excel2007FileToRead);
-        XSSFSheet xsheet = xwb.getSheetAt(0);
-        XSSFRow xrow;
-        XSSFCell xcell;
-        Iterator xrows = xsheet.rowIterator();
-        while (xrows.hasNext()) {
-            xrow = (XSSFRow) xrows.next();
-            Iterator xcells = xrow.cellIterator();
-            while (xcells.hasNext()) {
-                xcell = (XSSFCell) xcells.next();
-                if (xcell.getCellType() == XSSFCell.CELL_TYPE_STRING) {
-                    System.out.print(xcell.getStringCellValue() + " ");
-                } else if (xcell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC) {
-                    System.out.print(xcell.getNumericCellValue() + " ");
-                } else {
-                    //U Can Handel Boolean, Formula, Errors
-                }
-            }
-            System.out.println("");
-        }
-    }
-    
-    
-    public void readPDF(){
-        PDDocument pd;
-        BufferedWriter wr;
-        
-        try {
-         File input = new File("C:\\Invoice.pdf");  // The PDF file from where you would like to extract
-         File output = new File("C:\\SampleText.txt"); // The text file where you are going to store the extracted data
-         pd = PDDocument.load(input);
-         System.out.println(pd.getNumberOfPages());
-         System.out.println(pd.isEncrypted());
-         pd.save("CopyOfInvoice.pdf"); // Creates a copy called "CopyOfInvoice.pdf"
-         PDFTextStripper stripper = new PDFTextStripper();
-         stripper.setStartPage(3); //Start extracting from page 3
-         stripper.setEndPage(5); //Extract till page 5
-         wr = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output)));
-         stripper.writeText(pd, wr);
-         if (pd != null) {
-             pd.close();
-         }
-         
-        // I use close() to flush the stream.
-        wr.close();
-        } catch (Exception e){
-         e.printStackTrace();
-        } 
-     }
-       
     /*
     public static void main(String[] args) throws Exception {
 		String filename = "\\Users\\Manuu Alcocer\\Downloads\\salarios.xlsx";
@@ -200,25 +107,6 @@ public class FileUtilities {
     private static void insertQuery(String firstcolumnvalue,String secondcolumnvalue) {
 		System.out.println(firstcolumnvalue +  " "  +secondcolumnvalue);
     }
-*/
-    public static void main(String[] args) {
-        try {
-            File fileToRead = new File("\\Users\\Manuu Alcocer\\Downloads\\Plantillas_salida_Listasprecios.xlsx");
-            String fileToReadname = fileToRead.getName();
-            String extension = fileToReadname.substring(fileToReadname.lastIndexOf(".")
-                    + 1, fileToReadname.length());
-            String excel2003 = "xls";
-            String excel2007 = "xlsx";
-            if (excel2003.equalsIgnoreCase(extension)) {
-                FileInputStream Excel2003FileToRead = new FileInputStream(fileToRead);
-                readXLSFile(Excel2003FileToRead);
-            } else if (excel2007.equalsIgnoreCase(extension)) {
-                FileInputStream Excel2007FileToRead = new FileInputStream(fileToRead);
-                readXLSXFile(Excel2007FileToRead);
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+*/ 
     
-}
+}//fin
---
+++ src/controller/PDFUtils.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+/**
+ *
+ * @author Isaac Alcocer <aosi87@gmail.com>
+ */
+public class PDFUtils {
+    
+        public void readPDF(){
+        PDDocument pd;
+        BufferedWriter wr;
+        
+        try {
+         File input = new File("C:\\Invoice.pdf");  // The PDF file from where you would like to extract
+         File output = new File("C:\\SampleText.txt"); // The text file where you are going to store the extracted data
+         pd = PDDocument.load(input);
+         System.out.println(pd.getNumberOfPages());
+         System.out.println(pd.isEncrypted());
+         pd.save("CopyOfInvoice.pdf"); // Creates a copy called "CopyOfInvoice.pdf"
+         PDFTextStripper stripper = new PDFTextStripper();
+         stripper.setStartPage(3); //Start extracting from page 3
+         stripper.setEndPage(5); //Extract till page 5
+         wr = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output)));
+         stripper.writeText(pd, wr);
+         if (pd != null) {
+             pd.close();
+         }
+         
+        // I use close() to flush the stream.
+        wr.close();
+        } catch (Exception e){
+         e.printStackTrace();
+        } 
+     }
+       
+    
+}
