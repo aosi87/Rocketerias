bc68eb1262f595babd2078c5813cc35b8e2236d2
Isaac Alcocer <aosi87@gmail.com>
2014-06-14 22:41:52 -0500

    Se actualizaron los directorios.

--- src/controller/FileUtilities.java
+++ src/controller/FileUtilities.java
@@ -6,16 +6,25 @@
 
 package controller;
 
-    
+//JavaIO     
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+//Collections
 import java.util.ArrayList;
+import java.util.Iterator;
 // Actualice a ArrayList ya que tecnicamente Vector es obsoleto desde jdk 1.2
 //import java.util.Vector; 
 
+//Librerias para Apache PDFBox
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
 /* Librerias exclusivas de Apache POI */
-import java.util.Iterator;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -117,6 +126,35 @@ public class FileUtilities {
             System.out.println("");
         }
     }
+    
+    
+    public void readPDF(){
+        PDDocument pd;
+        BufferedWriter wr;
+        
+        try {
+         File input = new File("C:\\Invoice.pdf");  // The PDF file from where you would like to extract
+         File output = new File("C:\\SampleText.txt"); // The text file where you are going to store the extracted data
+         pd = PDDocument.load(input);
+         System.out.println(pd.getNumberOfPages());
+         System.out.println(pd.isEncrypted());
+         pd.save("CopyOfInvoice.pdf"); // Creates a copy called "CopyOfInvoice.pdf"
+         PDFTextStripper stripper = new PDFTextStripper();
+         stripper.setStartPage(3); //Start extracting from page 3
+         stripper.setEndPage(5); //Extract till page 5
+         wr = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output)));
+         stripper.writeText(pd, wr);
+         if (pd != null) {
+             pd.close();
+         }
+         
+        // I use close() to flush the stream.
+        wr.close();
+        } catch (Exception e){
+         e.printStackTrace();
+        } 
+     }
+       
     /*
     public static void main(String[] args) throws Exception {
 		String filename = "\\Users\\Manuu Alcocer\\Downloads\\salarios.xlsx";
